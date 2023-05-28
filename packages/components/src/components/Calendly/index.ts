4b381c41b02ae0482e29fd694dc5c64ae53ba868
Adam Harris <adam@lastrev.com>
2023-05-28 07:58:30 -0700

    Updated calendly export

--- packages/components/src/components/Calendly/index.ts
+++ packages/components/src/components/Calendly/index.ts
@@ -1,4 +1,1 @@
-export { default } from './CalendlyInline';
-export { default } from './CalendlyPopupWidget';
-export { default } from './CalendlyPopupButton';
 export type { CalendlyProps, CalendlyClasses, CalendlyClassKey } from './Calendly.types';
