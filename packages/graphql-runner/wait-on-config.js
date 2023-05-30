4414fbaf3716003b034132cf9651e00d69eaf9a4
Adam Harris <adam@lastrev.com>
2023-05-30 12:19:44 -0700

    Update wait on config

--- packages/graphql-runner/wait-on-config.js
+++ packages/graphql-runner/wait-on-config.js
@@ -1,10 +1,9 @@
 require('dotenv').config();
-const URL = `${(process.env.GRAPHQL_SERVER_URL || 'http://localhost:8888/graphql').replace(
-  /^http:/,
-  'http-get:'
-)}?query={__schema{types{name}}}`;
 
-const resource = `tcp:${URL?.split(':')?.pop()?.split('/')?.shift()}`;
+const baseUrl = 'http://localhost:8888/graphql';
+const URL = `${baseUrl.replace(/^http:/, 'http-get:')}?query={__schema{types{name}}}`;
+
+const resource = `tcp:${URL.split(':').pop().split('/').shift()}`;
 const timeout = process.env.GRAPHQL_SERVER_TIMEOUT ? parseInt(process.env.GRAPHQL_SERVER_TIMEOUT, 10) : 0;
 
 module.exports = {
